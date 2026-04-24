76cf9949619b50dc31084ce93714852f03c90f34
Ahmedshafie85 <abdelshafie.ahmed@gmail.com>
2026-04-24 13:04:15 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,18 @@
+// =============================================================
+// 🔥 FIREBASE CONFIGURATION
+// =============================================================
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBwsK7cJfbNcx_JMv6ogdyvjtszcKG37xI",
+  authDomain: "promos-maroc.firebaseapp.com",
+  projectId: "promos-maroc",
+  storageBucket: "promos-maroc.firebasestorage.app",
+  messagingSenderId: "56006761255",
+  appId: "1:56006761255:web:f8e2e5b6236a722e386702"
+};
+
+// =============================================================
+// 🔐 ADMIN EMAIL
+// =============================================================
+
+const ADMIN_EMAIL = "abdelshafie.ahmed@gmail.com";
