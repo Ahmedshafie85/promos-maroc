f5b4c40b31fbb86d511650f5cd8ae9c2116796e8
Ahmedshafie85 <abdelshafie.ahmed@gmail.com>
2026-04-25 14:08:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,6 +11,24 @@ const firebaseConfig = {
   appId: "1:56006761255:web:f8e2e5b6236a722e386702"
 };
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Services
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
 const ADMIN_EMAIL = "abdelshafie.ahmed@gmail.com";
 
+// --- YOUR NEW SCRAPING ENGINE URL ---
 const SMART_PASTE_WORKER_URL = "https://promos-maroc-fetcher.abdelshafie-ahmed.workers.dev";
+
+// =============================================================
+// 🌍 GLOBAL EXPORTS (Making them available to admin.html)
+// =============================================================
+window.db = db;
+window.auth = auth;
+window.storage = storage;
+window.ADMIN_EMAIL = ADMIN_EMAIL;
+window.SMART_PASTE_WORKER_URL = SMART_PASTE_WORKER_URL;
